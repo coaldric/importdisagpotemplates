3e7e6fe0c66233cccb169ec5ed421a6731bbb4e3
Cody Aldrich <57390775+coaldric@users.noreply.github.com>
2020-09-15 20:58:20 +0200

    Update Import-StigAsGPOs.ps1
    Removed Symbolic Link function;
    Set Extracted Path Statically
    
    DISA no longer has each individual file zipped/compressed so there is no longer an issue with character limits.

--- Import-StigAsGPOs.ps1
+++ Import-StigAsGPOs.ps1
@@ -1,16 +1,12 @@
-﻿<#
+<#
 .SYNOPSIS
     Import GPOs from DISA for STIG compliance. 
-
 .DESCRIPTION
     Import GPOs, WMI Filters, and Administrative Templates from DISA for STIG compliance. 
-
 .PARAMETER CompressedZip
     This is the file path of the compressed zip file downloaded from cyber.mil/stigs/gpo.
-
 .PARAMETER STIGPath
     This symbolic link will target the extracted files from the zip.
-
 .NOTES
     Author: Coaldric 2/4/2020
     Collaborators: JBear 5/24/2018 - Created the original script Import-STIGgpo (https://github.com/Average-Bear/Import-STIGgpo)
@@ -24,7 +20,6 @@
     Added function to select zip file and extract it. Then create a symbolic link to the extracted files which shortens the paths and avoids errors due to paths exceeding maximums.
     Added function to stage SYSVOL by copying Administrative Templats from the local repo (C:\Windows\PolicyDefinitions) and the Administrative Templates extracted from the zip to the SYSVOL.
     Added function to import WMI Filters from extracted files. 
-
     Upcoming Changes
     Would like to add more info so the user can see what's being imported in each stage and if everything was successful. 
     Would like to add try/catch for areas where failures can occur. 
@@ -46,132 +41,7 @@ param(
     
 )
 #transcript for logging purpases
-#Start-Transcript C:\log.txt -Verbose
-
-Function New-SymLink {
-    <#
-        .SYNOPSIS
-            Creates a Symbolic link to a file or directory
-
-        .DESCRIPTION
-            Creates a Symbolic link to a file or directory as an alternative to mklink.exe
-
-        .PARAMETER Path
-            Name of the path that you will reference with a symbolic link.
-
-        .PARAMETER SymName
-            Name of the symbolic link to create. Can be a full path/unc or just the name.
-            If only a name is given, the symbolic link will be created on the current directory that the
-            function is being run on.
-
-        .PARAMETER File
-            Create a file symbolic link
-
-        .PARAMETER Directory
-            Create a directory symbolic link
-
-        .NOTES
-            Name: New-SymLink
-            Author: Boe Prox
-            Created: 15 Jul 2013
-
-
-        .EXAMPLE
-            New-SymLink -Path "C:\users\admin\downloads" -SymName "C:\users\admin\desktop\downloads" -Directory
-
-            SymLink                          Target                   Type
-            -------                          ------                   ----
-            C:\Users\admin\Desktop\Downloads C:\Users\admin\Downloads Directory
-
-            Description
-            -----------
-            Creates a symbolic link to downloads folder that resides on C:\users\admin\desktop.
-
-        .EXAMPLE
-            New-SymLink -Path "C:\users\admin\downloads\document.txt" -SymName "SomeDocument" -File
-
-            SymLink                             Target                                Type
-            -------                             ------                                ----
-            C:\users\admin\desktop\SomeDocument C:\users\admin\downloads\document.txt File
-
-            Description
-            -----------
-            Creates a symbolic link to document.txt file under the current directory called SomeDocument.
-    #>
-    [cmdletbinding(
-        DefaultParameterSetName = 'Directory',
-        SupportsShouldProcess = $True
-    )]
-    Param (
-        [parameter(Position = 0, ParameterSetName = 'Directory', ValueFromPipeline = $True,
-            ValueFromPipelineByPropertyName = $True, Mandatory = $True)]
-        [parameter(Position = 0, ParameterSetName = 'File', ValueFromPipeline = $True,
-            ValueFromPipelineByPropertyName = $True, Mandatory = $True)]
-        [ValidateScript( {
-                If (Test-Path $_) { $True } Else {
-                    Throw "`'$_`' doesn't exist!"
-                }
-            })]
-        [string]$Path,
-        [parameter(Position = 1, ParameterSetName = 'Directory')]
-        [parameter(Position = 1, ParameterSetName = 'File')]
-        [string]$SymName,
-        [parameter(Position = 2, ParameterSetName = 'File')]
-        [switch]$File,
-        [parameter(Position = 2, ParameterSetName = 'Directory')]
-        [switch]$Directory
-    )
-    Begin {
-        Try {
-            $null = [mklink.symlink]
-        }
-        Catch {
-            Add-Type @"
-            using System;
-            using System.Runtime.InteropServices;
- 
-            namespace mklink
-            {
-                public class symlink
-                {
-                    [DllImport("kernel32.dll")]
-                    public static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
-                }
-            }
-"@
-        }
-    }
-    Process {
-        #Assume target Symlink is on current directory if not giving full path or UNC
-        If ($SymName -notmatch "^(?:[a-z]:\\)|(?:\\\\\w+\\[a-z]\$)") {
-            $SymName = "{0}\{1}" -f $pwd, $SymName
-        }
-        $Flag = @{
-            File      = 0
-            Directory = 1
-        }
-        If ($PScmdlet.ShouldProcess($Path, 'Create Symbolic Link')) {
-            Try {
-                $return = [mklink.symlink]::CreateSymbolicLink($SymName, $Path, $Flag[$PScmdlet.ParameterSetName])
-                If ($return) {
-                    $object = New-Object PSObject -Property @{
-                        SymLink = $SymName
-                        Target  = $Path
-                        Type    = $PScmdlet.ParameterSetName
-                    }
-                    $object.pstypenames.insert(0, 'System.File.SymbolicLink')
-                    $object
-                }
-                Else {
-                    Throw "Unable to create symbolic link!"
-                }
-            }
-            Catch {
-                Write-warning ("{0}: {1}" -f $path, $_.Exception.Message)
-            }
-        }
-    }
-}
+Start-Transcript C:\Import-STIG-Log.txt -Verbose
 
 #prompts user to select the compressed ZIP Path
 if ($compressedzip -eq $null) {
@@ -188,20 +58,16 @@ if ($compressedzip -eq $null) {
     if ($Result -eq 'OK') {
 
         Try {
-      
-            Expand-Archive -LiteralPath $dialog.FileName -DestinationPath "C:\" -force
-            $extractedzip = (Get-ChildItem -Path C:\*DISA*STIG*GPO*\).FullName
-            New-SymLink -Path "$extractedzip" -SymName "$STIGPath" -Directory
             
+            Expand-Archive -LiteralPath $dialog.FileName -DestinationPath "C:\Import-STIGasGPO" -force
+            $STIGPath = "C:\Import-STIGasGPO"
         }
-
         Catch {
 
             $compressedzip = $null
             Break
         }
     }
-
     else {
 
         #Shows upon cancellation of Save Menu
@@ -210,84 +76,82 @@ if ($compressedzip -eq $null) {
     }
 }
 
-
-
 #Imports the GPOs
 function Import-STIGasGPO {
     [Cmdletbinding(SupportsShouldProcess)]
     Param()
 
-    foreach ($STIG in $STIGPath) {
-
-        $BaseFile = (Get-ChildItem -Directory -Path $STIG\DoD*\GPOs\*).FullName
-                                
-    }
-
-    foreach ($Base in $BaseFile) {
+    $gpoTotal = 0
+    Write-Host -ForegroundColor Yellow "`n`nBeginning Group Policy Object Import."
     
-        [XML]$XML = (Get-Content $Base\Backup.xml)
-        $GPOName = $((Select-XML -XML $XML -XPath "//*").Node.DisplayName.'#cdata-section')
-        Import-GPO -Domain $($Domain) -BackupGpoName $GPOName -TargetName $GPOName -Path $base\.. -CreateIfNeeded
-    }
-}
+    # Import Policy Definitions
+    $defaultDefPath = "C:\Windows\PolicyDefinitions\"
+    $domainDefPath = "C:\Windows\SYSVOL\sysvol\$domain\PolicyDefinitions"
 
-#imports the WMI Filters (does not link them to GPOs)
-function Import-STIGWMIFilters {
-    [Cmdletbinding(SupportsShouldProcess)]
-    Param()
-
-    foreach ($STIG in $STIGPath) {
-
-        $WMIFilters = (Get-ChildItem -Path $STIG\DoD*\WMI*\*).FullName
-                                
-    }
-
-    foreach ($WMIFilter in $WMIFilters) {
-        #the .mofs from DISA need to be editted before being able to import them
-        (Get-Content $WMIFilter) -replace "security.local", "$domain" | Set-Content $WMIFilter 
-        (Get-Content $WMIFilter) -replace "gpoimport.local", "$domain" | Set-Content $WMIFilter 
-        #now all the .mofs will have the local domain info and will load properly
-        mofcomp -N:root\Policy $WMIFilter
-    }
-}
-
-#Stages the SYSVOL PolicyDefinitions folder with ADMXs and ADMLs from local C:\Windows and the DISA
-function Copy-AdministrativeTemplates {
-    [Cmdletbinding(SupportsShouldProcess)]
-    Param()
-    #copy local PolicyDefinitions to SYSVOL
-    copy-item -path C:\Windows\PolicyDefinitions\ -Destination C:\Windows\SYSVOL\sysvol\contoso.com\PolicyDefinitions -Recurse
-
-    foreach ($STIG in $STIGPath) {
-
-        $adminfiles = (Get-ChildItem -Path $STIG\ADMX*\*\ -Recurse).FullName
-                                
-    }
-
-    foreach ($admin in $adminFiles) {
-        #copy DISA ADMX/ADML files to SYSVOL           
-        Copy-Item -Path $admin -Include "*.admx" -Destination C:\Windows\SYSVOL\sysvol\contoso.com\PolicyDefinitions 
-        Copy-Item -Path $admin -include "*.adml" -Destination C:\Windows\SYSVOL\sysvol\contoso.com\PolicyDefinitions\en-us 
-               
-    }
-}
-
-<# Write-hosts for each GPO imported
-foreach($base in $basefile) {
-
-    $STIGImport = "Importing: $(Split-Path $base\..\..\ -Leaf)"
-
-    if(!([String]::IsNullOrWhiteSpace($MigrationTablePath))) {
+    if ((test-path $domainDefPath) -eq $false ) {
+        Write-Host -ForegroundColor Green "`tCopying Policy Definitions"
+        $null = copy-item -path $defaultDefPath -Destination $domainDefPath -Recurse
     
-        $MigrationFiles = "| Using $MigrationTablePath"
     }
 
-    Write-Host -ForegroundColor Yellow "`n$STIGImport $MigrationFiles"
+    # Import STIG GPOs
+    $StigTypes = Get-ChildItem -Path $STIGPath -Directory | Where-object { $_.name -notlike "ADMX Templates" }
+
+    foreach ($STIGType in $StigTypes ) {
+        $gpofiles = (Get-ChildItem -Directory -Path "$($STIGType.fullname)\GPOs\*").FullName
+        $wmiFilters = (Get-ChildItem -Path "$($STIGType.fullName)\WMI*\*").FullName
+        # $adminTemplates = (Get-ChildItem -Path "$($STIGType.fullName)\..\ADMX*\*\" -Recurse).FullName
+        $GpoTotal += $gpofiles.count 
+
+        Write-Host -ForegroundColor Yellow "Importing $($gpofiles.count) Group Policy objects for $($stigType.basename)" 
+        
+        foreach ($GPO in $gpofiles ) {
+            [XML]$XML = (Get-Content $GPO\Backup.xml)
+            $GPOName = $((Select-XML -XML $XML -XPath "//*").Node.DisplayName.'#cdata-section')
+            #$wmiFilters = (Get-Childitem -Path $GPO\WMI*).FullName
+            
+            Write-Host -ForegroundColor Yellow "`tImporting $GPOName"
+
+            # Import Group Policy Object
+            Write-Host -ForegroundColor Green "`t`tImporting Group Policy Object"
+            $null = Import-GPO -Domain $($Domain) -BackupGpoName $GPOName -TargetName $GPOName -Path $GPO\.. -CreateIfNeeded
+        }
+
+        foreach ($wmiFilter in $wmiFilters) {
+            (Get-Content $WMIFilter) -replace "security.local", "$domain" | Set-Content $WMIFilter 
+            (Get-Content $WMIFilter) -replace "gpoimport.local", "$domain" | Set-Content $WMIFilter 
+
+            # Import WMI Filters
+            Write-Host -ForegroundColor Green "`t`tImporting WMI Filters"
+            $null = mofcomp -N:root\Policy $WMIFilter
+           
+        }
+        $AdminTypes = Get-ChildItem -Path $STIGPath -Directory | Where-object { $_.name -like "ADMX Templates" }
+    
+        foreach ($AdminType in $AdminTypes) {
+            $adminTemplates = (Get-ChildItem -Path "$($AdminType.fullName)\*\" -Recurse).FullName
+            
+            # Import Administrative Templates
+            Write-Host -ForegroundColor Green "`t`tImporting Administrative Template Files."
+        
+            foreach ($adminTemplate in $adminTemplates ) {
+
+                $null = Copy-Item -Path "$adminTemplate" -Include "*.admx" -Destination "$domainDefPath"
+                $null = Copy-Item -Path "$adminTemplate"-include "*.adml" -Destination "$domainDefPath\en-us"
+            }
+        
+        }
+    }
+       
+
+    Write-Host -ForegroundColor Green "`n`nGroup Policy Import Complete!"
+    Write-Host -ForegroundColor Green "Total GPOs Imported - $GpoTotal"
+    Write-Host -ForegroundColor yellow "Performing Final Cleanup Actions: Removing all items stored under $STIGPath"        
 }
-#>
 
 #Calls functions; Import-STIGasGPO supports -WhatIf
 Import-STIGasGPO
-Import-STIGWMIFilters
-Copy-AdministrativeTemplates 
-#Stop-Transcript
+remove-item $STIGPath -Recurse
+Stop-Transcript
+Pause
+clear-host
